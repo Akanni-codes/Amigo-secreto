7ab813cafadd88133cb715c82793f1d1fbbc8e05
Akanni <akanni029@gmail.com>
2025-02-07 16:33:33 -0300

    first commit

---
+++ js/app.js
@@ -0,0 +1,12 @@
+function adicionar() {
+    let amigo = document.getElementById('nome-amigo');
+    let listaAmigos = document.getElementById('lista-amigos');
+
+    if (listaAmigos.textContent == ''){
+    listaAmigos.textContent = amigo.value;
+    } else {
+        listaAmigos.textContent = listaAmigos.textContent + ', ' + amigo.value;
+    }
+    amigo.value = '';
+}
+
